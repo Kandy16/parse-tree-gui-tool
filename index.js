16607754aaa7b178b595b8b1240688d19249cf3a
Denis Memmesheimer <kruskod@uni-koblenz.de>
2019-05-07 13:52:03 +0200

    Implemented load function

--- index.js
+++ index.js
@@ -1,218 +1,211 @@
 var bodyElem = d3.select('body'),
     jsElem = d3.select('#js'),
     jsPanel = bodyElem.append('div').attr('id', 'jsPanel');
-    cssElem = d3.select('#css'),
+cssElem = d3.select('#css'),
     cssPanel = bodyElem.append('div').attr('id', 'cssPanel');
 
 function setupPanel(panel, elem, title) {
-  panel.append('h2').text(title);
-  return panel.append('pre').append('code').text(elem.html().trim());
+    panel.append('h2').text(title);
+    return panel.append('pre').append('code').text(elem.html().trim());
 }
 
-var jsCode = setupPanel(jsPanel, jsElem, 'JavaScript');
-var cssCode = setupPanel(cssPanel, cssElem, 'CSS');
 
-var hljsRoot = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.1';
+var svg = d3.select('svg');
+var gNode = svg.select('g');
+var nodesCount = g.nodeCount();
+var edgesCount = g.edgeCount();
+console.log('Nodes count:', nodesCount);
 
-bodyElem.append('link')
-  .attr('rel', 'stylesheet')
-  .attr('href', hljsRoot + '/styles/xcode.min.css');
-bodyElem.append('script')
-  .attr('src', hljsRoot + '/highlight.min.js')
-  .on('load', function() {
-    hljs.highlightBlock(jsCode.node());
-    hljs.highlightBlock(cssCode.node());
-  });
-
-var svg = d3.select('svg')
-var gNode = svg.select('g')
-var nodesCount = g.nodeCount()
-var edgesCount = g.edgeCount()
-console.log('Nodes count:',nodesCount)
-
-var renderGraphics = function(){
+var renderGraphics = function () {
     render(d3.select("svg g"), g);
-}
-var selectedNodeIdentifier = undefined
+};
+var selectedNodeIdentifier = undefined;
 
-var allNodes = svg.selectAll('g.node')
+var allNodes = svg.selectAll('g.node');
 
-var selectNodeFunc = function(itemIndex){
-	
-	if(selectedNodeIdentifier != undefined) {
-		selectedItem.classed('showing', false);
-		console.log('Previous node:',selectedItem);		
-	}
-	
-    console.log('Selected Node index:',itemIndex);
-    selectedNodeIdentifier = itemIndex
-    selectedItem =  d3.select(this)
+var selectNodeFunc = function (itemIndex) {
+
+    if (selectedNodeIdentifier != undefined) {
+        selectedItem.classed('showing', false);
+        console.log('Previous node:', selectedItem);
+    }
+
+    console.log('Selected Node index:', itemIndex);
+    selectedNodeIdentifier = itemIndex;
+    selectedItem = d3.select(this);
     selectedItem.classed('showing', true)
-}
-allNodes.on('click',selectNodeFunc)
+};
+allNodes.on('click', selectNodeFunc);
 
-var deselectNodeFunc = function(itemIndex){
-    console.log('De-Selected Item index:',itemIndex);
+var deselectNodeFunc = function (itemIndex) {
+    console.log('De-Selected Item index:', itemIndex);
     selectedIdentifier = undefined;
     selectedItem = undefined;
     d3.select(this).classed('showing', false);
-}
-allNodes.on('blur',deselectNodeFunc)
+};
+allNodes.on('blur', deselectNodeFunc);
 
-var doubleClickNodeFunc = function(itemIndex){
-    console.log('Double clicked Node index:',itemIndex);
+var doubleClickNodeFunc = function (itemIndex) {
+    console.log('Double clicked Node index:', itemIndex);
     console.log(g.node(itemIndex))
-}
-allNodes.on('dblclick',doubleClickNodeFunc)
+};
+allNodes.on('dblclick', doubleClickNodeFunc);
 
-$("input[name='addnode']").click(function(){
-    console.log('Add node clicked')
-    if(selectedNodeIdentifier != undefined){
-        nodesCount  += 1
-        nodeString = nodesCount.toString()
-        g.setNode(nodesCount, {label:'node'+nodeString , 
-                       class:'type-'+nodeString });
-        edgesCount += 1
-        g.setEdge(selectedNodeIdentifier, nodesCount, {label:edgesCount.toString(), 
-                                                       class:'type-'+selectedNodeIdentifier+'_'+nodeString})
+$("input[name='addnode']").click(function () {
+    console.log('Add node clicked');
+    if (selectedNodeIdentifier != undefined) {
+        nodesCount += 1;
+        nodeString = nodesCount.toString();
+        g.setNode(nodesCount, {
+            label: 'node' + nodeString,
+            class: 'type-' + nodeString
+        });
+        edgesCount += 1;
+        g.setEdge(selectedNodeIdentifier, nodesCount, {
+            label: edgesCount.toString(),
+            class: 'type-' + selectedNodeIdentifier + '_' + nodeString
+        });
         //g.setEdge(2, 3, {label:'hi', class:'type-2_3'});
 
-        renderGraphics()
-        updateGraphSize()
-        
-        var newCreatedItem = gNode.select('.type-'+nodeString)
-        newCreatedItem.on('click',selectNodeFunc)
-        newCreatedItem.on('blur',deselectNodeFunc)
-        newCreatedItem.on('dblclick',doubleClickNodeFunc)
-        
-        var newCreatedEdge = gNode.select('.type-'+selectedNodeIdentifier+'_'+nodeString)
-        newCreatedEdge.on('focus',selectEdgeFunc)
-        
-        console.log(newCreatedItem)
-        console.log(newCreatedItem.node())
-        newCreatedItem.node().focus()
-        
-        console.log(newCreatedEdge)
+        renderGraphics();
+        updateGraphSize();
+
+        var newCreatedItem = gNode.select('.type-' + nodeString);
+        newCreatedItem.on('click', selectNodeFunc);
+        newCreatedItem.on('blur', deselectNodeFunc);
+        newCreatedItem.on('dblclick', doubleClickNodeFunc);
+
+        var newCreatedEdge = gNode.select('.type-' + selectedNodeIdentifier + '_' + nodeString);
+        newCreatedEdge.on('focus', selectEdgeFunc);
+
+        console.log(newCreatedItem);
+        console.log(newCreatedItem.node());
+        newCreatedItem.node().focus();
+
+        console.log(newCreatedEdge);
         console.log(newCreatedEdge.node())
-        
+
     }
-})
+});
 
-$("input[name='removenode']").click(function(){
-    console.log('Remove node clicked')
-    if(selectedNodeIdentifier != undefined){
-        
-        successorsList = g.successors(selectedNodeIdentifier)
-        predecessorsList = g.predecessors(selectedNodeIdentifier)
-        
-        console.log(predecessorsList)
-        console.log(successorsList)
+$("input[name='removenode']").click(function () {
+    console.log('Remove node clicked');
+    if (selectedNodeIdentifier != undefined) {
 
-        i = 0
-        while(i < successorsList.length){
-            tempList = g.successors(successorsList[i])
-            i = i + 1
-            
-            length = successorsList.length
-            for(j in tempList){
-                successorsList[length] = tempList[j]
+        successorsList = g.successors(selectedNodeIdentifier);
+        predecessorsList = g.predecessors(selectedNodeIdentifier);
+
+        console.log(predecessorsList);
+        console.log(successorsList);
+
+        i = 0;
+        while (i < successorsList.length) {
+            tempList = g.successors(successorsList[i]);
+            i = i + 1;
+
+            length = successorsList.length;
+            for (j in tempList) {
+                successorsList[length] = tempList[j];
                 length = length + 1
             }
         }
-        successorsList[length] = selectedNodeIdentifier
-        console.log(successorsList)
-        
-        for(i in successorsList){
+        successorsList[length] = selectedNodeIdentifier;
+        console.log(successorsList);
+
+        for (i in successorsList) {
             g.removeNode(successorsList[i])
         }
-        
+
         renderGraphics();
         updateGraphSize();
-        
-        selectedNodeIdentifier = undefined
+
+        selectedNodeIdentifier = undefined;
         //selectedItem = undefined
-        
-        if(predecessorsList.length > 0){
-            temp = predecessorsList[0]
-            tempItem = gNode.select('.type-'+temp)
+
+        if (predecessorsList.length > 0) {
+            temp = predecessorsList[0];
+            tempItem = gNode.select('.type-' + temp)
         }
-        if(tempItem != undefined){
-            console.log(tempItem)
-            console.log(tempItem.node())
+        if (tempItem != undefined) {
+            console.log(tempItem);
+            console.log(tempItem.node());
             tempItem.node().focus()
         }
 
     }
-})
+});
 
-$("input[name='left']").click(function(item){
-    console.log('move left clicked')
-    if(selectedNodeIdentifier != undefined){
-        console.log('There are nodes')
-        predecessors =  g.predecessors(selectedNodeIdentifier)
-        if(predecessors.length > 0){
-            console.log('there are predecessors')
-            parent = predecessors[0]
-            siblings = g.successors(parent)
-            if(siblings.length > 0){
-                console.log('there are siblings')
-                if(selectedNodeIdentifier != siblings[0]){
-                    console.log(selectedNodeIdentifier, siblings[0])
-                                        
-                    neighborList = []
-                    neighborLabelList = []
-                    selectedIndex = siblings.indexOf(selectedNodeIdentifier)
-                    console.log('Index of selected item:',selectedIndex)
-                    
-                    console.log(siblings)
+$("input[name='left']").click(function (item) {
+    console.log('move left clicked');
+    if (selectedNodeIdentifier != undefined) {
+        console.log('There are nodes');
+        predecessors = g.predecessors(selectedNodeIdentifier);
+        if (predecessors.length > 0) {
+            console.log('there are predecessors');
+            parent = predecessors[0];
+            siblings = g.successors(parent);
+            if (siblings.length > 0) {
+                console.log('there are siblings');
+                if (selectedNodeIdentifier != siblings[0]) {
+                    console.log(selectedNodeIdentifier, siblings[0]);
+
+                    neighborList = [];
+                    neighborLabelList = [];
+                    selectedIndex = siblings.indexOf(selectedNodeIdentifier);
+                    console.log('Index of selected item:', selectedIndex);
+
+                    console.log(siblings);
                     //swap current and previous
-                    temp = siblings[selectedIndex]
-                    siblings[selectedIndex] = siblings[selectedIndex-1]
-                    siblings[selectedIndex-1] = temp
-                    
-                    console.log(parent)
-                    console.log(siblings)
-                    
-                    siblingLabels = []
-                    for (i=0;i<siblings.length;i++){
-                        sibling = siblings[i]
-                        
-                        tmpLabel = g.edge(parent, sibling)['label']
-                        siblingLabels[i] = tmpLabel
-                        
-                        console.log(sibling, tmpLabel)
+                    temp = siblings[selectedIndex];
+                    siblings[selectedIndex] = siblings[selectedIndex - 1];
+                    siblings[selectedIndex - 1] = temp;
+
+                    console.log(parent);
+                    console.log(siblings);
+
+                    siblingLabels = [];
+                    for (i = 0; i < siblings.length; i++) {
+                        sibling = siblings[i];
+
+                        tmpLabel = g.edge(parent, sibling)['label'];
+                        siblingLabels[i] = tmpLabel;
+
+                        console.log(sibling, tmpLabel);
                         g.removeEdge(parent, sibling)
-                        
+
                     }
-                    
-                    setTimeout(renderGraphics, 1)
-                    setTimeout(updateGraphSize, 1)
-                    
-                    var updateFunction = function(i){
-                        
-                        return function(){
+
+                    setTimeout(renderGraphics, 1);
+                    setTimeout(updateGraphSize, 1);
+
+                    var updateFunction = function (i) {
+
+                        return function () {
                             console.log('Update function executing after few ms');
                             console.log(siblings);
                             //for (i=0;i<siblings.length;i++){
-                                sibling = siblings[i];
-                                siblingLabel = siblingLabels[i];
-                                console.log(sibling, siblingLabel);
-                                g.setEdge(parent, sibling, {'label':i.toString(), 'class':'type-'+parent+'_'+sibling+' extra'});    
-                                //break;
+                            sibling = siblings[i];
+                            siblingLabel = siblingLabels[i];
+                            console.log(sibling, siblingLabel);
+                            g.setEdge(parent, sibling, {
+                                'label': i.toString(),
+                                'class': 'type-' + parent + '_' + sibling + ' extra'
+                            });
+                            //break;
                             //}
 
                             renderGraphics();
                             updateGraphSize();
                         }
-                    }
-                    setTimeout(updateFunction(0), 1000+i*2000)
-                    setTimeout(updateFunction(2), 1000+i*2000)
-                    setTimeout(updateFunction(1), 1000+i*2000)
-                    for (i=0;i<siblings.length;i++){ 
+                    };
+                    setTimeout(updateFunction(0), 1000 + i * 2000);
+                    setTimeout(updateFunction(2), 1000 + i * 2000);
+                    setTimeout(updateFunction(1), 1000 + i * 2000);
+                    for (i = 0; i < siblings.length; i++) {
                         //setTimeout(updateFunction(i), 1000+i*1000)
                     }
-                    
-                } else  {
+
+                } else {
                     console.log('It is at left extreme!!!')
                 }
             } else {
@@ -221,80 +214,91 @@ $("input[name='left']").click(function(item){
         } else {
             console.log('No parents!!!')
         }
-    } else{
+    } else {
         console.log('No nodes selected !!!')
     }
-})
+});
 
 
-var allEdges = svg.selectAll('g.edgePath')
-var selectedEdgeIdentifier = undefined
-var selectedEdge = undefined
-var recipientNodeIdentifier = undefined
-var selectEdgeFunc = function(edgeIndex){
-    console.log('Selected edge index:',edgeIndex);
+var allEdges = svg.selectAll('g.edgePath');
+var selectedEdgeIdentifier = undefined;
+var selectedEdge = undefined;
+var recipientNodeIdentifier = undefined;
+var selectEdgeFunc = function (edgeIndex) {
+    console.log('Selected edge index:', edgeIndex);
     selectedEdgeIdentifier = edgeIndex;
-    selectedEdge =  d3.select(this);
+    selectedEdge = d3.select(this);
     selectedEdge.classed('showing', true);
-}
-allEdges.on('focus',selectEdgeFunc)
+};
+allEdges.on('focus', selectEdgeFunc);
 
-$("input[name='selectrecipientnode']").click(function(){
-    console.log('select recipient node clicked')
-    if(selectedNodeIdentifier != undefined){
-        
+$("input[name='selectrecipientnode']").click(function () {
+    console.log('select recipient node clicked');
+    if (selectedNodeIdentifier != undefined) {
+
         recipientNodeIdentifier = selectedNodeIdentifier
 
     }
-})
+});
 
-$("input[name='addedge']").click(function(){
-    console.log('Add edge clicked')
-    if(recipientNodeIdentifier != undefined & selectedNodeIdentifier != undefined){
-        edgesCount += 1
-        g.setEdge(selectedNodeIdentifier, recipientNodeIdentifier, {label:edgesCount.toString(), 
-                                                                    class:'type-'+selectedNodeIdentifier+'_'+recipientNodeIdentifier})
-        
+$("input[name='addedge']").click(function () {
+    console.log('Add edge clicked');
+    if (recipientNodeIdentifier != undefined & selectedNodeIdentifier != undefined) {
+        edgesCount += 1;
+        g.setEdge(selectedNodeIdentifier, recipientNodeIdentifier, {
+            label: edgesCount.toString(),
+            class: 'type-' + selectedNodeIdentifier + '_' + recipientNodeIdentifier
+        });
+
         renderGraphics();
         updateGraphSize();
-        
-        var newCreatedEdge = gNode.select('.type-'+selectedNodeIdentifier+'_'+recipientNodeIdentifier)
-        newCreatedEdge.on('focus',selectEdgeFunc)
-        
+
+        var newCreatedEdge = gNode.select('.type-' + selectedNodeIdentifier + '_' + recipientNodeIdentifier);
+        newCreatedEdge.on('focus', selectEdgeFunc);
+
         recipientNodeIdentifier = undefined
-        
+
 
     }
-})
+});
 
-$("input[name='removeedge']").click(function(){
-    console.log('Remove edge clicked')
-    if(selectedEdgeIdentifier != undefined){
-        
-        g.removeEdge(selectedEdgeIdentifier['v'], selectedEdgeIdentifier['w'])
-        
+$("input[name='removeedge']").click(function () {
+    console.log('Remove edge clicked');
+    if (selectedEdgeIdentifier != undefined) {
+
+        g.removeEdge(selectedEdgeIdentifier['v'], selectedEdgeIdentifier['w']);
+
         renderGraphics();
         updateGraphSize();
-        
+
         selectedEdgeIdentifier = undefined
         //selectedEdge = undefined
 
     }
-})
+});
 
-$("input[name='loadtree']").click(function(){
+$("input[name='loadtree']").click(function () {
     console.log('Load tree clicked');
- 
-})
- 
-$("input[name='savetree']").click(function(){
-    console.log('Save tree clicked');
-    console.log(dagreD3.graphlib.json.write(g))
-    console.log(g.toJson())
- 
-})
+    const parse_str = document.querySelector("#input").value.trim().replace(/(\r\n|\n|\r)/gm, "").replace(/\s+/g,' ');
+    if (parse_str.startsWith('(') && parse_str.endsWith(')')) {
+        const tree = parseBrackets(parse_str);
+        if (tree) {
+            var parentDiv = document.querySelector("#parses");
+            const div = parentDiv.appendChild(create_graph_div(tree));
+            const svg = div.appendChild(document.createElementNS("http://www.w3.org/2000/svg", "svg"));
+            drawTree(tree, svg);
+        }
+    }
+});
 
-function updateGraphSize(){
+$("input[name='savetree']").click(function () {
+    console.log('Save tree clicked');
+    console.log(dagreD3.graphlib.json.write(g));
+    console.log(g.toJson())
+
+});
+
+function updateGraphSize() {
     var xCenterOffset = (svg.attr("width") - g.graph().width) / 2;
     svgGroup.attr("transform", "translate(" + xCenterOffset + ", 20)");
     svg.attr("height", g.graph().height + 40);
---
+++ tree.js
@@ -0,0 +1,300 @@
+//creates a new div for a new parse
+function create_graph_div(tree) {
+    let div = document.createElement("DIV");
+    let pre = document.createElement("PRE");
+    pre.innerHTML = parse2str(tree);
+    div.appendChild(pre);
+    return div;
+}
+
+// // form request to cgi and parse response
+// function parse() {
+//     const parentDiv = document.querySelector("#parses");
+//     //clean previous parses
+//     removeAllChildren(parentDiv);
+//
+//     const request = new XMLHttpRequest();
+//     request.open('GET', build_cgi_query(), true);
+//
+//     request.onload = function () {
+//         let msg = '';
+//         if (request.status >= 200 && request.status < 400) {
+//             if (isBlank(request.responseText)) {
+//                 msg = 'No parses were found';
+//             } else {
+//                 let lines = request.responseText.trim().split('\n');
+//                 for (let i in lines) {
+//                     const parse_str = lines[i].trim();
+//                     if (parse_str.startsWith('(') && parse_str.endsWith(')')) {
+//                         const tree = parseBrackets(parse_str);
+//                         if (tree) {
+//                             const div = parentDiv.appendChild(create_graph_div(tree));
+//                             const svg = div.appendChild(document.createElementNS("http://www.w3.org/2000/svg", "svg"));
+//                             drawTree(tree, svg);
+//                         }
+//
+//                     }
+//                 }
+//             }
+//         } else {
+//             // We reached our target server, but it returned an error
+//             if (request.status === 0) {
+//                 msg = 'Not connect. Verify Network.';
+//             } else if (request.status === 404) {
+//                 msg = 'Requested page not found. [404]';
+//             } else if (request.status === 500) {
+//                 msg = 'Internal Server Error [500].';
+//             } else {
+//                 msg = 'Uncaught Error.' + request.responseText;
+//             }
+//
+//         }
+//         document.querySelector("#parseStatus strong").innerHTML = msg;
+//     };
+//
+//     request.onerror = function () {
+//         // There was a connection error of some sort
+//         console.log("An error occurred during the requerst:", request);
+//     };
+//     request.send();
+// }
+
+// visualize parse tree using dagre-d3
+function build_graph(graph, tree, startindex = 1) {
+    let label = tree.label;
+    if (tree.morph) {
+        label += '-' + tree.morph
+    }
+    const graph_node = {label: label,};
+    if (tree.class) {
+        graph_node.class = tree.class
+    }
+    graph.setNode(startindex, graph_node);
+    let child_index = startindex * 10;
+    let children = tree.children;
+    if (children) {
+        for (let i in children) {
+            let child = children[i];
+            build_graph(graph, child, child_index);
+            const child_edge = {};
+            if (child.gf) {
+                child_edge.label = child.gf;
+            }
+            graph.setEdge(startindex, child_index, child_edge);
+            child_index += 1;
+        }
+    }
+}
+
+// use dagre and d3 to draw a tree on the svg_elem
+function drawTree(tree, svg_elem) {
+    // Create the input graph
+    // Available options: https://github.com/dagrejs/dagre/wiki
+    const g = new dagreD3.graphlib.Graph()
+        .setGraph({nodesep: 30, ranksep: 30})
+        //      .setGraph({})
+        .setDefaultEdgeLabel(function () {
+            return {};
+        });
+
+    build_graph(g, tree);
+
+    g.nodes().forEach(function (v) {
+        let node = g.node(v);
+        // Round the corners of the nodes
+        node.rx = node.ry = 5;
+    });
+
+    // Create the renderer
+    const render = new dagreD3.render();
+
+    // Set up an SVG group so that we can translate the final graph.
+    const svg = d3.select(svg_elem),
+        svgGroup = svg.append("g");
+
+    // Run the renderer. This is what draws the final graph.
+    render(d3.select(svg_elem.firstChild), g);
+
+    svg.attr("width", g.graph().width + 40);
+    // Center the graph
+    const xCenterOffset = (svg.attr("width") - g.graph().width) / 2;
+    svgGroup.attr("transform", "translate(" + xCenterOffset + ", 20)");
+    svg.attr("height", g.graph().height + 40);
+}
+
+/*
+    Takes a nonterminal in form 'PH-GF-MORPH' and returns an object
+    {
+        label: PH,
+        gf: GF,
+        morph: MORPH,
+    }
+ */
+function parseNonterminal(ntstr) {
+    const ntarray = ntstr.trim().split('-');
+    if (ntarray.length === 0) {
+        console.log("Unable to parse node:", ntstr);
+        return null;
+    }
+    const node = {label: ntarray[0]};
+
+    if (ntarray.length >= 2) {
+        const gf = ntarray[1].trim();
+        if (gf !== 'NONE' && gf !== 'NONE/nohead') {
+            node.gf = gf;
+        }
+    }
+
+    if (ntarray.length >= 3) {
+        const morph = ntarray[2].trim();
+        if (morph !== 'NONE') {
+            node.morph = morph;
+        }
+    }
+    return node;
+}
+
+// append a new child to the parent and set backreference child.parent
+function add_node(parent, child) {
+    if (parent) {
+        if (!parent.hasOwnProperty('children')) {
+            parent.children = []
+        }
+        parent.children.push(child);
+        child.parent = parent;
+    }
+}
+
+// returns string representaiton of a node
+function node2str(node) {
+    let result = node.label;
+    if (node.gf) {
+        result += '-' + node.gf;
+    }
+    if (node.morph) {
+        result += '-' + node.morph;
+    }
+    return result;
+}
+
+// returns neat formatted string representation of a parse
+function parse2str(tree, level = 0) {
+    if (!tree) {
+        return;
+    }
+    const children = tree.children;
+    // check if the tree is a terminal
+    if (!children) {
+        return node2str(tree);
+    }
+    // tree is a nonterminal
+    let result = '(' + node2str(tree);
+    const spacing = '\t'.repeat(level + 1);
+    if (children.length > 1) {
+        for (let i in children) {
+            result += '\n' + spacing + parse2str(children[i], level + 1);
+        }
+    } else {
+        result += ' ' + parse2str(children[0], level);
+    }
+    return result + ')';
+}
+
+/*
+    (TOP (SIMPX-NONE/nohead (VF-NONE/nohead (NX-ON (PPER-HD Es)))(LK-NONE (VXFIN-HD (VAFIN-HD ist)))(MF-NONE/nohead (NX-OA (ART-NONE eine)(ADJX-NONE (ADJA-HD schöne))(NN-HD Frau)))))
+    transforms parse string in bracketing format (as above) to an object
+ */
+function parseBrackets(brackets) {
+    let unmatched_brackets = 0;
+    let parentobj = null;
+    let node = '';
+    for (let i in brackets) {
+        let ch = brackets.charAt(i);
+
+        switch (ch) {
+            case '(':
+                unmatched_brackets += 1;
+                if (!isEmpty(node)) {
+                    let nonterminal = parseNonterminal(node);
+                    add_node(parentobj, nonterminal);
+                    parentobj = nonterminal;
+                    node = '';
+                }
+                break;
+            case ')':
+                unmatched_brackets -= 1;
+                if (!isEmpty(node)) {
+                    let termArray = node.trim().split(/(\s+)/);
+                    if (termArray.length > 1) {
+                        const preterm = parseNonterminal(termArray[0]);
+                        const term = termArray.slice(1).join(' ').trim();
+                        add_node(preterm, {label: term, class: "type-TK"});
+                        add_node(parentobj, preterm);
+                    }
+                    node = '';
+                } else {
+                    if (unmatched_brackets) { // top element has no parent
+                        parentobj = parentobj.parent;
+                    }
+                }
+                break;
+            default:
+                node += ch;
+        }
+    }
+    console.assert(unmatched_brackets === 0);
+//    console.log(parentobj);
+    return parentobj;
+}
+
+// utility functions
+
+function removeAllChildren(elem) {
+    while (elem.firstChild) {
+        elem.removeChild(elem.firstChild);
+    }
+}
+
+function isEmpty(str) {
+    return (!str || 0 === str.length);
+}
+
+function isBlank(str) {
+    return (!str || /^\s*$/.test(str));
+}
+
+function getUrlVars() {
+    let vars = {};
+    const hashes = window.location.search.substring(1).split('&');
+    for (let i in hashes) {
+        let hash = hashes[i].split('=');
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+function setInputFields(vars) {
+    if (!vars) {
+        return;
+    }
+    const elems = document.querySelectorAll("input[type=number],input[type=text]");
+    for (let i in elems) {
+        let elem = elems[i];
+        if (vars[elem.name]) {
+            elem.value = decodeURIComponent(vars[elem.name].replace(/\+/g, " "));
+        }
+    }
+}
+
+// window.onload = function () {
+//     setInputFields(getUrlVars());
+//     parse();
+// };
+
+function catch_enter(event, function_on_event) {
+    if (event.which == 13 || event.keyCode == 13) {
+        function_on_event();
+        return false;
+    }
+    return true;
+}
